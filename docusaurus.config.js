d22d7316201c16806388d50c3b3034ccc3fafea0
Joel Barrett <joelbarrett50@hotmail.com>
2023-10-03 20:59:48 +0900

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Visionary Render Docs',
+  tagline: '',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'virtalis', // Usually your GitHub org/user name.
+  projectName: 'visren-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Visionary Render Logo',
+          src: 'img/visren-logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'userManualSidebar',
+            position: 'left',
+            label: 'User Manual',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'progGuideSidebar',
+            position: 'left',
+            label: 'Programming Guide',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Virtalis. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['c', 'cmake', 'glsl', 'lua', 'xml-doc'],
+      },
+    }),
+};
+
+module.exports = config;
